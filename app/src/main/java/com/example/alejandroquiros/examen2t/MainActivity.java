4643429f50a772926b1708350ad90ce3752294a4
Alex-Kun <alejandro.quiros@live.u-tad.com>
2018-02-19 11:39:29 +0100

    Animacion añadida al boton de facebook! FADE

--- app/src/main/java/com/example/alejandroquiros/examen2t/MainActivity.java
+++ app/src/main/java/com/example/alejandroquiros/examen2t/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.alejandroquiros.examen2t;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -22,12 +27,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+
+
+
+
+
+
+
         //FACEBOOK
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
         // If using in a fragment
         //loginButton.setFragment(this);
+
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setDuration(1000);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(1000);
+        fadeOut.setDuration(1000);
+
+        AnimationSet animation = new AnimationSet(false); //change to false
+        animation.addAnimation(fadeIn);
+        animation.addAnimation(fadeOut);
+        loginButton.setAnimation(animation);
+
+
+
+
+
+
 
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
